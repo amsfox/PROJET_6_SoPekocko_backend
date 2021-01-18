e942081f26e5bd902e01d4fc5f7a907abc3e59d3
amsfox <kaba.ahmed@me.com>
2021-01-18 21:48:39 +0100

    suppression commentaires inutiles

--- controllers/user.js
+++ controllers/user.js
@@ -16,7 +16,7 @@ exports.signup = (req, res, next) => {
     return res.status(400).json({ 'error': 'email is not valid' });
   };
   if (REGEX_INJECT.test(password)) {
-    return res.status(400).json({ 'error': 'password invalid must no inclue "=}{' });
+    return res.status(400).json({ 'error': 'password invalid must no include "=}{' });
   };
   if (!PASSWORD_REGEX.test(password)) {
     return res.status(400).json({ 'error': 'password invalid (must length 4 - 8 and include 1 number at least)' });
@@ -58,47 +58,3 @@ exports.login = (req, res, next) => {
     })
     .catch(error => res.status(500).json({ error }));
 };
-
-
-// backup
-
-/*
-exports.signup = (req, res, next) => {
-  bcrypt.hash(req.body.password, 10)
-    .then(hash => {
-      const user = new User({
-        email: req.body.email,
-        password: hash
-      });
-      user.save()
-        .then(() => res.status(201).json({ message: 'Utilisateur créé !' }))
-        .catch(error => res.status(400).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
-};
-
-exports.login = (req, res, next) => {
-  User.findOne({ email: req.body.email })
-    .then(user => {
-      if (!user) {
-        return res.status(401).json({ error: 'Utilisateur non trouvé !' });
-      }
-      bcrypt.compare(req.body.password, user.password)
-        .then(valid => {
-          if (!valid) {
-            return res.status(401).json({ error: 'Mot de passe incorrect !' });
-          }
-          res.status(200).json({
-            userId: user._id,
-            token: jwt.sign(
-              { userId: user._id },
-              'RANDOM_TOKEN_SECRET',
-              { expiresIn: '24h' }
-            )
-          });
-        })
-        .catch(error => res.status(500).json({ error }));
-    })
-    .catch(error => res.status(500).json({ error }));
-};
-*/
